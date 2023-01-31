b6e9092ca0d20f9ea218acc436450628f4f8a77e
plamyx <albanese.aurelien06@gmail.com>
2023-01-31 16:47:22 +0100

    script debug

--- src/script/global.js
+++ src/script/global.js
@@ -1,14 +1,16 @@
-document.getElementById("navbar__menu--js").addEventListener("click", () => {
-  let navOpen = document.getElementById("navbar__js--open");
-  let navClose = document.getElementById("navbar__js--close");
-  navOpen.style.display = "block";
-  navClose.style.display = "none";
-});
-document
-  .getElementById("navbar__menu--js--open")
-  .addEventListener("click", () => {
+window.onload = function () {
+  document.getElementById("navbar__menu--js").addEventListener("click", () => {
     let navOpen = document.getElementById("navbar__js--open");
     let navClose = document.getElementById("navbar__js--close");
-    navOpen.style.display = "none";
-    navClose.style.display = "";
+    navOpen.style.display = "block";
+    navClose.style.display = "none";
   });
+  document
+    .getElementById("navbar__menu--js--open")
+    .addEventListener("click", () => {
+      let navOpen = document.getElementById("navbar__js--open");
+      let navClose = document.getElementById("navbar__js--close");
+      navOpen.style.display = "none";
+      navClose.style.display = "";
+    });
+};
