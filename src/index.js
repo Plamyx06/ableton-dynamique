f781a001754ac8ec840e0e655e2e4ef9544adb69
plamyx <albanese.aurelien06@gmail.com>
2023-02-13 18:41:45 +0100

    index.js simplifié, global.js refait et ajout des attribut

--- src/index.js
+++ src/index.js
@@ -1,102 +1,148 @@
-const nunjucks = require("nunjucks");
-const data = require("./data/index.json");
-const fsp = require("fs/promises");
-const dataLogin = require("./data/login.json");
-const { minify } = require("html-minifier-terser");
-const cleanCSS = require("clean-css-promise");
-const terser = require("terser");
-const minifyJS = terser.minify;
+import nunjucks from "nunjucks";
+import fsp from "fs/promises";
+import { minify } from "html-minifier-terser";
+import cleanCSS from "clean-css-promise";
+import { minify as minifyJS } from "terser";
+import path from "path";
+const [dataIndex, dataLogin] = await Promise.all([
+  fsp
+    .readFile("./src/data/index.json", "utf8")
+    .then((data) => JSON.parse(data)),
+  fsp
+    .readFile("./src/data/login.json", "utf8")
+    .then((data) => JSON.parse(data)),
+]);
 
-const args = process.argv.slice(2).join("");
-const boolArgsContainDev = argsContainDev(args);
-const minifiedHtmlOption = {
-  removeAttributeQuotes: true,
-  collapseWhitespace: true,
-  removeComments: true,
+const args = process.argv.slice(2);
+const isDev = args[0] === "dev";
+const optionJsMinified = {
+  compress: {
+    drop_console: true,
+    unsafe: true,
+  },
+  mangle: {
+    reserved: ["jQuery"],
+  },
+  output: {
+    comments: "some",
+    beautify: false,
+  },
 };
 
 async function main() {
+  await fsp.rm("./dist", { recursive: true, force: true });
+  await fsp.mkdir("./dist");
+  await fsp.mkdir("./dist/member");
+  const templateIndex = nunjucks.render("./src/template/index.njk", dataIndex);
+  const templateLogin = nunjucks.render("./src/template/login.njk", dataLogin);
+
+  if (isDev) {
+    Promise.all([
+      copyAllFiles(
+        [
+          "./src/css/index.css",
+          "./src/css/global.css",
+          "./src/script/global.js",
+          "./src/css/login.css",
+        ],
+        [
+          "./dist/index.css",
+          "./dist/global.css",
+          "./dist/global.js",
+          "./dist/member/login.css",
+        ]
+      ),
+      fsp.writeFile("./dist/index.html", templateIndex),
+      console.log("index.html file created"),
+      fsp.writeFile("./dist/member/login.html", templateLogin),
+      console.log(`login.html file created`),
+    ]);
+  } else {
+    Promise.all([
+      minifyHTML(templateIndex, "./dist/index.html"),
+      minifyHTML(templateLogin, "./dist/member/login.html"),
+      minifiedCSSorJS(
+        [
+          "./src/css/index.css",
+          "./src/css/global.css",
+          "./src/css/login.css",
+          "./src/script/global.js",
+        ],
+        [
+          "./dist/index.css",
+          "./dist/global.css",
+          "./dist/member/login.css",
+          "./dist/global.js",
+        ]
+      ),
+    ]);
+  }
+}
+
+main();
+
+async function minifyHTML(template, dist) {
+  const minifiedHtmlOption = {
+    removeAttributeQuotes: true,
+    collapseWhitespace: true,
+    removeComments: true,
+  };
   try {
-    await fsp.rm("./dist", { recursive: true, force: true });
-    await fsp.mkdir("./dist");
-    const templateIndex = nunjucks.render("./src/template/index.njk", data);
-    const minifiedIndex = await minify(templateIndex, minifiedHtmlOption);
-    if (boolArgsContainDev === true) {
-      await (fsp.writeFile("./dist/index.html", templateIndex) &&
-        console.log(`index.html file created`));
-    } else {
-      await (fsp.writeFile("./dist/index.html", minifiedIndex) &&
-        console.log(`index.html file has been minified created`));
-    }
-    const dataIndexCSS = await fsp.readFile("./src/css/index.css", "utf8");
-    const minifiedIndexCSS = await new cleanCSS().minify(dataIndexCSS);
-    if (boolArgsContainDev === true) {
-      await (fsp.copyFile("./src/css/index.css", "./dist/index.css") &&
-        console.log("The index.css file copied!"));
-    } else {
-      await (fsp.writeFile("./dist/index.css", minifiedIndexCSS.styles) &&
-        console.log("The index.css file has been minified and copied!"));
-    }
-    const dataGlobalCSS = await fsp.readFile("./src/css/global.css", "utf8");
-    const minifiedGlobalCSS = await new cleanCSS().minify(dataGlobalCSS);
-    if (boolArgsContainDev === true) {
-      await (fsp.copyFile("./src/css/global.css", "./dist/global.css") &&
-        console.log("The global.css file copied!"));
-    } else {
-      await (fsp.writeFile("./dist/global.css", minifiedGlobalCSS.styles) &&
-        console.log("The global.css file has been minified and copied!"));
-    }
-    await fsp.mkdir("./dist/member");
-    let templateLogin = nunjucks.render("./src/template/login.njk", dataLogin);
-    let minifiedLogin = await minify(templateLogin, minifiedHtmlOption);
-    if (boolArgsContainDev === true) {
-      await (fsp.writeFile("./dist/member/login.html", templateLogin) &&
-        console.log(`login.html file created`));
-    } else {
-      await (fsp.writeFile("./dist/member/login.html", minifiedLogin) &&
-        console.log(`login.html file minified and created`));
-    }
-    const dataLoginCSS = await fsp.readFile("./src/css/login.css", "utf8");
-    const minifiedLoginCSS = await new cleanCSS().minify(dataLoginCSS);
-    if (boolArgsContainDev === true) {
-      await (fsp.copyFile("./src/css/login.css", "./dist/member/login.css") &&
-        console.log("The login.css file copied!"));
-    } else {
-      await (fsp.writeFile(
-        "./dist/member/login.css",
-        minifiedLoginCSS.styles
-      ) && console.log("The login.css file has been minified and copied!"));
-    }
-    if (boolArgsContainDev === true) {
-      await (fsp.copyFile("./src/script/global.js", "./dist/global.js") &&
-        console.log("global.js file copied"));
-    } else {
-      const globalJS = await fsp.readFile("./src/script/global.js", "utf-8");
-      const globalJSMinified = await minifyJS(globalJS, {
-        compress: {
-          drop_console: true,
-          unsafe: true,
-        },
-        mangle: {
-          reserved: ["jQuery"],
-        },
-        output: {
-          comments: "some",
-          beautify: false,
-        },
-      }).then((result) => result.code);
-      await fsp.writeFile("./dist/global.js", globalJSMinified);
-      console.log(`global.js file minified and copied!`);
-    }
+    const minified = await minify(template, minifiedHtmlOption);
+    await fsp.writeFile(dist, minified);
+    console.log(
+      `The file ${path.basename(dist)} has been minified and created.`
+    );
   } catch (err) {
     throw err;
   }
 }
 
-function argsContainDev(str) {
-  if (str === "dev" || str === "Dev") {
-    return true;
-  } else return false;
+async function minifiedCSSorJS(srcArray, distArray) {
+  for (let i = 0; i < srcArray.length; i++) {
+    const fileExtension = path.extname(srcArray[i]);
+
+    if (fileExtension === ".css") {
+      Promise.all([
+        fsp
+          .readFile(srcArray[i], "utf8")
+          .then((dataCSS) => new cleanCSS().minify(dataCSS))
+          .then((minifiedCSS) =>
+            fsp.writeFile(distArray[i], minifiedCSS.styles)
+          )
+          .then(() =>
+            console.log(
+              `The ${path.basename(
+                srcArray[i]
+              )} file has been minified and copied!`
+            )
+          ),
+      ]);
+    } else if (fileExtension === ".js") {
+      Promise.all([
+        fsp
+          .readFile(srcArray[i], "utf8")
+          .then((dataJS) => minifyJS(dataJS, optionJsMinified))
+          .then((JSMinified) => fsp.writeFile(distArray[i], JSMinified.code))
+          .then(() =>
+            console.log(
+              `The ${path.basename(
+                srcArray[i]
+              )} file has been minified and copied!`
+            )
+          ),
+      ]);
+    } else {
+      throw new err(`${fileExtension} not supported`);
+    }
+  }
 }
 
-main();
+async function copyAllFiles(srcArray, distArray) {
+  for (let i = 0; i < srcArray.length; i++) {
+    await fsp.copyFile(srcArray[i], distArray[i]);
+    console.log(
+      `The file ${path.basename(srcArray[i])} has been successfully copied!`
+    );
+  }
+}
--- src/script/global.js
+++ src/script/global.js
@@ -1,16 +1,37 @@
-window.onload = function () {
-  document.getElementById("navbar__menu--js").addEventListener("click", () => {
-    let navOpen = document.getElementById("navbar__js--open");
-    let navClose = document.getElementById("navbar__js--close");
-    navOpen.style.display = "block";
-    navClose.style.display = "none";
+window.addEventListener("DOMContentLoaded", () => {
+  const menuButton = document.getElementById("menuButton");
+  let navOpenElement = document.getElementById("navbar__js--open");
+  let allNavbarElement = document.getElementById("navbarAll");
+  let logoElement = document.getElementById("main-nav__logo__image--js");
+
+  menuButton.addEventListener("click", () => {
+    const isOpen = menuButton.getAttribute("data-open") === "true";
+
+    if (isOpen) {
+      closeNavbar();
+    } else {
+      openNavbar();
+    }
   });
-  document
-    .getElementById("navbar__menu--js--open")
-    .addEventListener("click", () => {
-      let navOpen = document.getElementById("navbar__js--open");
-      let navClose = document.getElementById("navbar__js--close");
-      navOpen.style.display = "none";
-      navClose.style.display = "";
-    });
-};
+
+  function openNavbar() {
+    navOpenElement.style.display = "block";
+    navOpenElement.style.zIndex = "1";
+    menuButton.style.color = "white";
+    logoElement.style.fill = "white";
+    navOpenElement.style.color = "white";
+    allNavbarElement.style.backgroundColor = "blue";
+    navOpenElement.style.backgroundColor = "blue";
+    menuButton.setAttribute("data-open", true);
+  }
+
+  function closeNavbar() {
+    navOpenElement.style.display = "none";
+    menuButton.style.color = "";
+    logoElement.style.fill = "";
+    navOpenElement.style.color = "";
+    allNavbarElement.style.backgroundColor = "";
+    navOpenElement.style.backgroundColor = "";
+    menuButton.setAttribute("data-open", false);
+  }
+});
