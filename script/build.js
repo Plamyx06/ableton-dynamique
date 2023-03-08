6bafbe9d5d8a62ca99fc31fffcba2966bf70f652
plamyx <albanese.aurelien06@gmail.com>
2023-03-08 09:05:07 +0100

    build & server

--- script/build.js
+++ script/build.js
@@ -11,109 +11,97 @@ nunjucks.configure({ autoescape: true });
 const args = process.argv.slice(2);
 const isDev = args[0] === "dev";
 
-const srcFileCSSToDest = [
-  {
-    src: "./src/css/index.css",
-    dist: "./dist/index.css",
-  },
-  {
-    src: "./src/css/global.css",
-    dist: "./dist/global.css",
-  },
-  {
-    src: "./src/css/login.css",
-    dist: "./dist/login.css",
-  },
-  {
-    src: "./src/css/article.css",
-    dist: "./dist/blog/article.css",
-  },
-];
-const srcFileJSToDest = [
-  {
-    src: "./src/script/global.js",
-    dist: "./dist/global.js",
-  },
-];
-const srcJson = {
-  index: "./src/data/index.json",
-  login: "./src/data/login.json",
-  global: "./src/data/global.json",
-  article: "./src/data/articles.json",
-};
-const srcTemplate = {
-  index: "./src/template/index.njk",
-  login: "./src/template/login.njk",
-  blog: "./src/template/blog.njk",
-};
+const dataIndex = await getDataIndex(
+  "./src/data/index.json",
+  "./src/data/global.json",
+  "./src/data/articles.json"
+);
+const dataLogin = await mergedDataWithGlobal("./src/data/login.json");
+const data404 = await mergedDataWithGlobal("./src/data/404.json");
 
 async function main() {
   await fs.rm("./dist", { recursive: true, force: true });
   await fs.mkdir("./dist");
 
   await Promise.all([
-    handleCSS(srcFileCSSToDest),
-    njkToHtml(srcTemplate.index, "./dist/index.html", srcJson),
-    njkToHtml(srcTemplate.login, "./dist/login.html", srcJson),
-    handleJS(srcFileJSToDest),
-    handleArticlePages(srcTemplate.blog, "./dist/blog"),
+    handleCSS([
+      {
+        src: "./src/css/index.css",
+        dist: "./dist/index.css",
+      },
+      {
+        src: "./src/css/global.css",
+        dist: "./dist/global.css",
+      },
+      {
+        src: "./src/css/login.css",
+        dist: "./dist/login.css",
+      },
+      {
+        src: "./src/css/article.css",
+        dist: "./dist/blog/article.css",
+      },
+      {
+        src: "./src/css/articlesIndex.css",
+        dist: "./dist/blog/articlesIndex.css",
+      },
+      {
+        src: "./src/css/404.css",
+        dist: "./dist/404.css",
+      },
+    ]),
+    handleJS([
+      {
+        src: "./src/script/global.js",
+        dist: "./dist/global.js",
+      },
+    ]),
+    njkToHtml("./src/template/index.njk", "./dist/index.html", dataIndex),
+    njkToHtml(
+      "./src/template/indexArticles.njk",
+      "./dist/blog/index.html",
+      dataLogin
+    ),
+    njkToHtml("./src/template/login.njk", "./dist/login.html", dataLogin),
+    njkToHtml("./src/template/404.njk", "./dist/404.html", data404),
+    handleArticlesIndex("./dist/blog/index.html"),
+    handleArticlePages("./src/template/article.njk"),
   ]);
 }
 main();
-
-async function handleArticlePages(templatePath, destPath) {
-  if (!(await pathExists(destPath))) {
-    await fs.mkdir(destPath, { recursive: true });
-  }
-  const [articlesData, dataGlobal] = await Promise.all([
-    readJsonFile("./src/data/articles.json"),
-    readJsonFile("./src/data/global.json"),
-  ]);
-  const articlesHighlight = articlesData.slice(0, 3);
-
-  const articlesHighlightWithUrls = articlesHighlight.map((article) => {
-    const openGraphUrl = `/blog/${slugify(article.title)}-${article.id}.html`;
-    return {
-      ...article,
-      openGraphUrl,
-    };
-  });
-
-  for (const article of articlesData) {
-    const dest = `${destPath}/${slugify(article.title)}-${article.id}.html`;
+async function handleArticlesIndex(destPath) {
+  const dataArticles = await addOpengraphUrlToArticle(
+    "./src/data/articles.json"
+  );
+  const dataGlobal = await readJsonFile("./src/data/global.json");
+  const dataArticlesIndex = await readJsonFile("./src/data/articlesIndex.json");
+  const mergedData = { dataArticles, ...dataGlobal, ...dataArticlesIndex };
+  await njkToHtml("./src/template/indexArticles.njk", destPath, mergedData);
+}
+async function handleArticlePages(templatePath) {
+  const dataGlobal = await readJsonFile("./src/data/global.json");
+  const dataArticles = await addOpengraphUrlToArticle(
+    "./src/data/articles.json"
+  );
+  for (const article of dataArticles) {
     const data = {
       head: {
         titleTab: article.title,
-        openGraphUrl: dest,
+        openGraphUrl: article.openGraphUrl,
         openGraphTitle: article.title,
         openGraphDescription: article.description,
         openGraphPicture: article.image,
         CSSglobal: "/global.css",
         CSS: "article.css",
       },
-      articlesHighlightWithUrls,
       article,
       ...dataGlobal,
     };
-    const htmlPage = nunjucks.render(templatePath, data);
-    if (isDev) {
-      await fs.writeFile(dest, htmlPage);
-    } else {
-      const minified = await minify(htmlPage, {
-        removeAttributeQuotes: true,
-        collapseWhitespace: true,
-        removeComments: true,
-      });
-      await fs.writeFile(dest, minified);
-    }
+    const dest = `./dist${data.head.openGraphUrl}.html`;
+
+    await njkToHtml(templatePath, dest, data);
   }
-  if (isDev) {
-    console.info(`you have created ${articlesData.length} articles page html`);
-  } else {
-    console.info(
-      `you have created ${articlesData.length} articles page html minified`
-    );
-  }
+  console.info(`you have created ${dataArticles.length} articles page html`);
 }
 async function pathExists(path) {
   try {
@@ -163,39 +151,45 @@ async function handleCSS(arrOfSrcAndDest) {
     if (!(await pathExists(path.dirname(dist)))) {
       await fs.mkdir(path.dirname(dist), { recursive: true });
     }
-    if (!isDev) {
+    if (isDev) {
+      await fs.copyFile(src, dist);
+      console.info(`The ${path.basename(src)} file has been copied!`);
+    } else {
       const dataCSS = await fs.readFile(src, "utf8");
       const minifiedCSS = await new cleanCSS().minify(dataCSS);
       await fs.writeFile(dist, minifiedCSS.styles);
-
       console.info(
         `The ${path.basename(src)} file has been minified and copied!`
       );
-    } else {
-      await fs.copyFile(src, dist);
-      console.info(`The ${path.basename(src)} file has been copied!`);
     }
   }
 }
-async function njkToHtml(templatePath, dest, dataJson) {
-  const filename = path.basename(dest, ".html");
+async function createArticleSlug(title, id) {
+  return `${slugify(title)}-${id}`;
+}
+async function mergedDataWithGlobal(pathData) {
+  const [data, dataGlobal] = await Promise.all([
+    readJsonFile(pathData),
+    readJsonFile("./src/data/global.json"),
+  ]);
 
-  const data = await readJsonFile(dataJson[filename]);
-  const dataGlobal = await readJsonFile(dataJson.global);
+  const mergedData = { ...data, ...dataGlobal };
+  return mergedData;
+}
+async function getDataIndex(pathDataIndex, pathDataGlobal, pathDataArticles) {
+  const [dataIndex, dataGlobal] = await Promise.all([
+    readJsonFile(pathDataIndex),
+    readJsonFile(pathDataGlobal),
+  ]);
+  const dataArticles = await addOpengraphUrlToArticle(pathDataArticles);
+  const articlesHighlight = dataArticles.slice(0, 3);
 
-  if (path.basename(dest) === "index.html") {
-    const dataArticles = await readJsonFile(dataJson.article);
-    const articlesHighlight = [];
-    for (const article of dataArticles.slice(0, 3)) {
-      const openGraphUrl = `/blog/${slugify(article.title)}-${article.id}.html`;
-      const articleWithHighlight = { ...article, openGraphUrl };
-      articlesHighlight.push(articleWithHighlight);
-    }
-    data.articlesHighlight = articlesHighlight;
-  }
+  const mergedData = { ...dataGlobal, ...dataIndex, articlesHighlight };
 
-  const mergedData = { ...dataGlobal, ...data };
-  const htmlPage = nunjucks.render(templatePath, mergedData);
+  return mergedData;
+}
+async function njkToHtml(templatePath, dest, data) {
+  const htmlPage = nunjucks.render(templatePath, data);
 
   if (!(await pathExists(path.dirname(dest)))) {
     await fs.mkdir(path.dirname(dest), { recursive: true });
@@ -203,16 +197,26 @@ async function njkToHtml(templatePath, dest, dataJson) {
 
   if (isDev) {
     await fs.writeFile(dest, htmlPage);
-    console.log(`${path.basename(dest)} is created!`);
   } else {
     const minified = await minify(htmlPage, {
       removeAttributeQuotes: true,
       collapseWhitespace: true,
       removeComments: true,
     });
+
     await fs.writeFile(dest, minified);
-    console.log(
-      `The file ${path.basename(dest)} has been minified and created.`
-    );
   }
+}
+async function addOpengraphUrlToArticle(pathArticles) {
+  const dataArticles = await readJsonFile(pathArticles);
+  const articles = [];
+  for (const article of dataArticles) {
+    const openGraphUrl = `/blog/${await createArticleSlug(
+      article.title,
+      article.id
+    )}`;
+    const articleWithUrl = { ...article, openGraphUrl };
+    articles.push(articleWithUrl);
+  }
+  return articles;
 }
--- script/server.js
+++ script/server.js
@@ -6,43 +6,46 @@ import path from "path";
 
 const server = http.createServer(async (request, response) => {
   let contentType = "text/html"
-
-
   if (request.method === "GET") {
-    let filePath = "./dist" + request.url;
-    console.log({ requestURL })
+
+    const url = request.url;
+    let filePath = `./dist${url}`;
+
     if (filePath.endsWith(".css")) {
       contentType = "text/css";
     } else if (filePath.endsWith(".js")) {
       contentType = "application/javascript";
+    }
 
-    } else if (!filePath.endsWith(".html")) {
-      filePath = filePath + ".html"
+    console.log(await isDir(filePath))
+    if (await isDir(filePath)) {
+      filePath += "/index";
     }
-    if (request.url === "/login") {
-      filePath = filePath + ".html"
+    if (url === "/blog/404.css") {
+      filePath = "./dist/404.css"
+    }
+    if (!filePath.endsWith(".js") && !filePath.endsWith(".css")) {
+      filePath = filePath + ".html";
+    }
 
-    }
-    if (request.url === filePath.endsWith(".html")) {
-      filePath = "./dist/global.js";
-    }
-    console.log({ filePath })
     if ((await pathExists(filePath)) && (await isFile(filePath))) {
 
       const content = await fs.readFile(filePath, "utf-8");
       response.setHeader("Content-type", contentType);
-
       response.end(content);
     } else {
+      const content = await fs.readFile("./dist/404.html", "utf-8");
+      response.setHeader("Content-type", contentType);
       response.statusCode = 404;
-      response.end("<h1>Page not found</h1>");
+      response.end(content);
     }
   }
-});
+}
+);
 
-server.listen(3000)//, () => {
-//console.log("http://localhost:3000");
-//});
+server.listen(3000), () => {
+  console.log("http://localhost:3000");
+}
 
 async function pathExists(path) {
   try {
@@ -52,27 +55,20 @@ async function pathExists(path) {
     return false;
   }
 }
-
 async function isFile(filePath) {
-  const stats = await fs.stat(filePath);
-  return stats.isFile();
-}
-async function removeExtensionsHTML(filePath) {
-  const fileExtension = path.extname(filePath);
-  console.log({ fileExtension })
-  if (fileExtension === ".html") {
-    const fileName = path.basename(filePath, fileExtension);
-    const pathWithoutExtension = path.join(path.dirname(filePath), fileName);
-    return pathWithoutExtension;
+  try {
+    const stat = await fs.stat(filePath);
+    return stat.isFile();
+  } catch (error) {
+    return false;
   }
-  else return filePath
-
 }
+
 async function isDir(filePath) {
-  const stats = await fs.stat(filePath);
-  return stats.isDirectory();
+  try {
+    const stat = await fs.stat(filePath);
+    return stat.isDirectory();
+  } catch (error) {
+    return false;
+  }
 }
-
-/*file path sans extension!
-si c'est un dossier -> / -> index.html 
-si c'est un fichier -> filepath + ".html"*/
